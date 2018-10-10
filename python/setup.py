755743cdd20f4ccbc7599297a1b130d7b61adbfa
Renuka Fernando <renuka.piyumal@gmail.com>
2018-10-10 08:15:13 +0530

    update python readme file

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,13 @@
+# Copyright (c) 2018 Renuka Fernando
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from setuptools import setup, find_packages
+
+with open("README", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="hackgen",
@@ -7,11 +16,11 @@ setup(
     author_email="renukapiyumal@gmail.com",
     license="MIT",
     description="HackerRank Test-Case Generator",
+    long_description=long_description,
     url="https://github.com/renuka-fernando/hackgen",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
